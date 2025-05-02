f78af2b2e712a9c00d596e49b3a194d7287fa98e
Ash Hellwig <ahellwig.dev@gmail.com>
2025-05-02 16:43:38 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.CommandLine;
+
+namespace gkp;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var outputDirectory = new Option<DirectoryInfo?>(
+            name: "--directory",
+            description: "Output directory for the key files."
+        );
+    }
+}
